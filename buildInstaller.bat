df5f6663b0504e7a284a097f07176601cffe8656
Dmitry Korneev <st.lynx@gmail.com>
2012-09-18 15:55:41 +0400

    Небольшое изменение с файлсе скрипта построения дистрибьютива

--- buildInstaller.bat
+++ buildInstaller.bat
@@ -4,4 +4,4 @@ cd ..\..\
 
 .\Installer\makensis.exe .\Installer\Relkon6.nsi
 set /p ver=<version
-move /Y .\SetupRelkon.exe .\SetupRelkon%ver%.exe
+move /Y .\SetupRelkon.exe .\SetupRelkon-%ver%.exe

07737c65c9df30a6e477a0d6e7fde67d5811346a
Dmitry Korneev <st.lynx@gmail.com>
2012-10-09 10:15:38 +0400

    Добавлен недостающий файл ControllerDispatcheringVar.cs

---
+++ IDE/Kontel.Components/Classes/ControllerDispatcheringVar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Kontel.Relkon.Classes
+{
+    public sealed class ControllerDispatcheringVar : ControllerVar
+    {     
+    }
+}

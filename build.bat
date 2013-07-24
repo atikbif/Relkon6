98b476a7ec3d5a5b32e80bc57ab4d7942b464fec
Dmitriy Korneev <st.lynx@gmail.com>
2013-07-24 11:24:52 +0400

    Версия 6.4

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 cls
 color 0a
 
-.\versionUpdater.exe 6 3 03.08.2012
+.\versionUpdater.exe 6 4 24.07.2013
 
 %windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe .\IDE\Relkon6.sln /t:Rebuild /p:Configuration=Release
 
